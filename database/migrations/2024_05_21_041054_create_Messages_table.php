080b428fd1fc7cde88cb1a3be2a3746c38e07d96
sangla3 <lavansang1264@gmail.com>
2024-06-04 23:08:02 +0700

    update

--- database/migrations/2024_05_21_041054_create_Messages_table.php
+++ database/migrations/2024_05_21_041054_create_Messages_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Messages', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->foreignId('conversation_id')->constrained();
 
@@ -35,6 +35,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('Messages');
+        Schema::dropIfExists('messages');
     }
 };
